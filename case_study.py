15fd3720ad5c9d939bdb0b24cf2d4cfeadab0ca9
Melisa Çevik <celissamevik@gmail.com>
2024-01-29 21:47:32 +0300

    add

--- case_study.py
+++ case_study.py
@@ -11,9 +11,8 @@ pd.set_option('display.width', 500)
 # Soru 1:
 df = pd.read_csv("datasets/persona.csv")
 
-# Soru 2 + Soru 3 + Soru 4 + Soru 5
+# Soru 2 + Soru 4 + Soru 5
 
-df["SOURCE"].unique()
 def col_frequency_ratio(dataframe, col_name):
     value_counts = dataframe[col_name].value_counts()
     ratios = value_counts / len(dataframe[col_name]) * 100
@@ -21,28 +20,31 @@ def col_frequency_ratio(dataframe, col_name):
     total_count = value_counts.sum()
     total_ratio = ratios.sum()
 
-    result_df = pd.DataFrame({ col_name : value_counts, "Ratio" : ratios})
-    result_df.loc["Total"] = [total_count, total_ratio]
+    result_df = pd.DataFrame({ col_name : value_counts,
+                               "Ratio" : ratios})
+
+   # result_df.loc["Total"] = [total_count, total_ratio]
 
     return result_df
 
 
 result_source = col_frequency_ratio(df,"SOURCE")
-result_price = col_frequency_ratio(df,"PRICE")
 result_country = col_frequency_ratio(df,"COUNTRY")
 
 print(result_source)
-print(result_price)
 print(result_country)
+
+# Soru 3 :
+df["PRICE"].nunique()
 
 # Soru 6
 
-country_total_price = df.groupby(["PRICE", "COUNTRY"]).size().reset_index(name='Sales_Count')
+country_total_price = df.groupby(["PRICE", "COUNTRY"]).size().reset_index(name='Sales_Count') # df series di => reset index data frame e çevirdi
 
 country_total_price['Total_Price'] = country_total_price['PRICE'] * country_total_price['Sales_Count']
 
 total_earnings_by_country = country_total_price.groupby("COUNTRY")["Total_Price"].sum().reset_index(name='Total_Earnings')
-
+type(total_earnings_by_country)
 def total_earnings_by_col_name(dataframe,group_col_name, col_name):
     col_total_price = dataframe.groupby([group_col_name, col_name]).size().reset_index(name='Sales_Count')
     col_total_price['Total_Price'] = col_total_price[group_col_name] * col_total_price['Sales_Count']
@@ -53,12 +55,12 @@ def total_earnings_by_col_name(dataframe,group_col_name, col_name):
 total_earnings_by_country = total_earnings_by_col_name(df, "PRICE","COUNTRY")
 
 # Soru 7
-source_total_count = df.groupby(["SOURCE", "COUNTRY"]).size().reset_index(name='Sales_Count')
+df.groupby(["SOURCE", "COUNTRY"]).size()
 
 # Soru 8 + Soru 9
 
-country_price_mean = df.groupby("COUNTRY")["PRICE"].mean().reset_index(name="Price_Mean")
-source_price_mean = df.groupby("SOURCE")["PRICE"].mean().reset_index(name="Source_Mean")
+df.groupby("COUNTRY")["PRICE"].mean()
+df.groupby("SOURCE")["PRICE"].mean()
 
 # function
 def col_groupcol_col(dataframe,group_col,col):
@@ -69,12 +71,13 @@ country_price_mean = col_groupcol_col(df, "COUNTRY", "PRICE")
 
 # Soru 10
 
-pivot_table = pd.pivot_table(df, values='PRICE', index='COUNTRY', columns='SOURCE', aggfunc='mean', fill_value=0).reset_index()
-print(pivot_table)
+df.groupby(["COUNTRY","SOURCE"])["PRICE"].mean()
 
     ###################################### GÖREV 2 #####################################
 
-average_gains = df.groupby(["COUNTRY", "SOURCE", "SEX", "AGE"]).agg(PRICE=('PRICE', 'mean'))
+average_gains = df.groupby(["COUNTRY", "SOURCE", "SEX", "AGE"])["PRICE"].mean()
+
+df.groupby(['COUNTRY', 'SOURCE', 'SEX', 'AGE'], as_index=False)['PRICE'].mean()
 
    ###################################### GÖREV 3 #####################################
 
