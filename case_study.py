008cf98cc9634bdd0491e4b3ad3d3540ddcf6c1c
Melisa Çevik <celissamevik@gmail.com>
2024-01-30 11:31:00 +0300

    add Question 8

--- case_study.py
+++ case_study.py
@@ -8,11 +8,33 @@ pd.set_option('display.width', 500)
 
      ###################################### GÖREV 1 #####################################
 
-# Soru 1:
+#Soru 1: persona.csv dosyasını okutunuz ve veri seti ile ilgili genel bilgileri gösteriniz.
+
 df = pd.read_csv("datasets/persona.csv")
 
-# Soru 2 + Soru 4 + Soru 5
 
+#Soru 2: Kaç unique SOURCE vardır? Frekansları nedir?
+df.nunique()
+
+#Soru 3:Kaç unique PRICE vardır?
+df["PRICE"].nunique() #6 tane eşsiz değer var.
+
+#Soru 4:Hangi PRICE'dan kaçar tane satış gerçekleşmiş?
+print("PRICE sütununun frekansları:")
+print(df["PRICE"].value_counts())
+print("\n")
+
+#Soru 5:Hangi ülkeden kaçar tane satış olmuş?
+df["COUNTRY"].value_counts()
+
+# Tüm kolonların sayıları için fonksiyon
+
+for col in df.columns:
+    print(f"{col} sütununun frekansları:")
+    print(df[col].value_counts())
+    print("\n")
+
+# frekans ve oranlar için fonksiyon
 def col_frequency_ratio(dataframe, col_name):
     value_counts = dataframe[col_name].value_counts()
     ratios = value_counts / len(dataframe[col_name]) * 100
@@ -34,61 +56,57 @@ result_country = col_frequency_ratio(df,"COUNTRY")
 print(result_source)
 print(result_country)
 
-# Soru 3 :
-df["PRICE"].nunique()
+#Soru 6: Ülkelere göre satışlardan toplam ne kadar kazanılmış?
 
-# Soru 6
+df.groupby("COUNTRY")["PRICE"].sum()
 
-country_total_price = df.groupby(["PRICE", "COUNTRY"]).size().reset_index(name='Sales_Count') # df series di => reset index data frame e çevirdi
 
-country_total_price['Total_Price'] = country_total_price['PRICE'] * country_total_price['Sales_Count']
-
-total_earnings_by_country = country_total_price.groupby("COUNTRY")["Total_Price"].sum().reset_index(name='Total_Earnings')
-type(total_earnings_by_country)
+# Toplamları için Fonksiyon hali!
 def total_earnings_by_col_name(dataframe,group_col_name, col_name):
-    col_total_price = dataframe.groupby([group_col_name, col_name]).size().reset_index(name='Sales_Count')
-    col_total_price['Total_Price'] = col_total_price[group_col_name] * col_total_price['Sales_Count']
-    total_earnings = col_total_price.groupby(group_col_name)["Total_Price"].sum().reset_index(name=f'{col_name}_Total_Earnings')
+        total_earnings = dataframe.groupby(group_col_name)[col_name].sum()
+        return total_earnings
 
-    return total_earnings
+total_earnings_by_country = total_earnings_by_col_name(df, "COUNTRY","PRICE")
 
-total_earnings_by_country = total_earnings_by_col_name(df, "PRICE","COUNTRY")
-
-# Soru 7
+#Soru 7: SOURCE türlerine göre satış sayıları nedir?
 df.groupby(["SOURCE", "COUNTRY"]).size()
 
-# Soru 8 + Soru 9
+# Soru 8:Ülkelere göre PRICE ortalamaları nedir?
 
 df.groupby("COUNTRY")["PRICE"].mean()
+
+# Soru 9:SOURCE'lara göre PRICE ortalamaları nedir?
+
 df.groupby("SOURCE")["PRICE"].mean()
 
-# function
+# ORTALAMALARI için function
 def col_groupcol_col(dataframe,group_col,col):
     col_price_mean = dataframe.groupby(group_col)[col].mean()
     return col_price_mean
 
 country_price_mean = col_groupcol_col(df, "COUNTRY", "PRICE")
 
-# Soru 10
+# Soru 10: COUNTRY-SOURCE kırılımında PRICE ortalamaları nedir?
 
 df.groupby(["COUNTRY","SOURCE"])["PRICE"].mean()
 
     ###################################### GÖREV 2 #####################################
-
-average_gains = df.groupby(["COUNTRY", "SOURCE", "SEX", "AGE"])["PRICE"].mean()
-
+#Görev2: COUNTRY, SOURCE, SEX, AGE kırılımında ortalama kazançlar nedir?
+#1. çözüm
+df.groupby(["COUNTRY", "SOURCE", "SEX", "AGE"])["PRICE"].mean()
+#2. çözüm
 df.groupby(['COUNTRY', 'SOURCE', 'SEX', 'AGE'], as_index=False)['PRICE'].mean()
 
    ###################################### GÖREV 3 #####################################
+#Görev 3:  Çıktıyı PRICE’a göre sıralayınız.
 
-agg_df = average_gains.sort_values(by="PRICE", ascending=False).reset_index()
+agg_df = df.groupby(["COUNTRY","SOURCE","SEX","AGE"])["PRICE"].mean()
+agg_df.sort_values(ascending=False)
 
 ###################################### GÖREV 4 #####################################
+#Görev 4:  Indekste yer alan isimleri değişken ismine çeviriniz.
 
-df.set_index('AGE', inplace=True)
-
-df = df.rename_axis("AGE").reset_index()
-
+agg_df = agg_df.reset_index()
 
 ###################################### GÖREV 5 #####################################
 
@@ -112,19 +130,15 @@ customers_level_based = pd.DataFrame({
 })
 
 ###################################### GÖREV 7 #####################################
-agg_df["PRICE"] = pd.to_numeric(agg_df["PRICE"])  # PRICE serisi sayısal veri tipine çevrildi
 
-price_bins = [0, 10, 20, 30, 40, 50, 60]
-price_labels = ['F', 'E', 'D', 'C', 'B', 'A']
+agg_df["SEGMENT"] = pd.qcut(agg_df["PRICE"], 4, labels=["D", "C", "B", "A"])
+print(agg_df)
 
-agg_df['SEGMENT'] = pd.cut(agg_df['PRICE'], bins=price_bins, labels=price_labels, right=False)  # sağ sınırı kabul etme
+agg_df.groupby("SEGMENT").agg({"PRICE": ["mean", "max", "sum"]})
 
-group_with_segment = agg_df.groupby("SEGMENT")["PRICE"].agg(["mean", "std", "min", "max"])
+#Görev 8:  Yeni gelen müşterileri sınıflandırıp, ne kadar gelir getirebileceklerini  tahmin ediniz
 
-group_with_segment.dropna()
-
-
-
-
-
+agg_df[agg_df['customer_level_based'].str.contains("TUR_ANDROID_FEMALE_31_40")]['PRICE'].mean()
+agg_df[agg_df['customer_level_based'].str.contains("FRA_IOS_FEMALE_31_40")]['PRICE'].mean()
+agg_df[agg_df['customer_level_based'].str.contains("USA_IOS_MALE_41_46")]['PRICE'].mean()
 
